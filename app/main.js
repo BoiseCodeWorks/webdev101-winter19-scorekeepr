98a94ce76c794c261ececb06a74ea5f24d03dea1
Darryl Kilzer <darrylkilzer@gmail.com>
2019-11-06 20:10:55 -0700

    rom(again)

--- app/main.js
+++ app/main.js
@@ -6,7 +6,21 @@ let player2 = {
 }
 let players = [player1, player2]
 
+let player1Elem = document.querySelector("#count1")
+let player2Elem = document.querySelector("#count2")
+
+let finalScore = 11
+
 function clicked(playerIndex) {
     players[playerIndex].score++
-    console.log(players[playerIndex].score)
+    player1Elem.textContent = players[0].score.toString()
+    player2Elem.textContent = players[1].score.toString()
 }
+
+function winValidation() {
+    if (players[0].score == finalScore) {
+
+    }
+}
+
+
